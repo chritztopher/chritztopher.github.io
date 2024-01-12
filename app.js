6c9ff73fef03d4e7622e4deac35927801909bfea
chritztopher <156460702+chritztopher@users.noreply.github.com>
2024-01-12 13:32:39 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,101 +1,32 @@
-const filter_btns = document.querySelectorAll(".filter-btn");
-const skills_wrap = document.querySelector(".skills");
-const skills_bars = document.querySelectorAll(".skill-progress");
-const records_wrap = document.querySelector(".records");
-const records_numbers = document.querySelectorAll(".number");
-const footer_input = document.querySelector(".footer-input");
-const hamburger_menu = document.querySelector(".hamburger-menu");
-const navbar = document.querySelector("header nav");
-const links = document.querySelectorAll(".links a");
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
 
-footer_input.addEventListener("focus", () => {
-  footer_input.classList.add("focus");
-});
-
-footer_input.addEventListener("blur", () => {
-  if (footer_input.value != "") return;
-  footer_input.classList.remove("focus");
-});
-
-function closeMenu() {
-  navbar.classList.remove("open");
-  document.body.classList.remove("stop-scrolling");
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle('show')
+        })
+    }
 }
 
-hamburger_menu.addEventListener("click", () => {
-  if (!navbar.classList.contains("open")) {
-    navbar.classList.add("open");
-    document.body.classList.add("stop-scrolling");
-  } else {
-    closeMenu();
-  }
-});
+showMenu('nav-toggle','nav-menu')
 
-links.forEach((link) => link.addEventListener("click", () => closeMenu()));
+/*----- ANIMATE -----*/
+// OVERLAY
+gsap.to(".first", 1.5, {delay: .5, top: "-100%", ease: Expo.easeInOut});
+gsap.to(".second", 1.5, {delay: .7, top: "-100%", ease: Expo.easeInOut});
+gsap.to(".third", 1.5, {delay: .9, top: "-100%", ease: Expo.easeInOut});
 
-filter_btns.forEach((btn) =>
-  btn.addEventListener("click", () => {
-    filter_btns.forEach((button) => button.classList.remove("active"));
-    btn.classList.add("active");
+// IMG
+gsap.from('.home__img', {opacity: 0, duration: 2, delay: 2, x: 60})
 
-    let filterValue = btn.dataset.filter;
+// INFORMATION
+gsap.from('.home__information', {opacity: 0, duration: 3, delay: 2.3, y: 25})
+gsap.from('.anime-text', {opacity: 0, duration: 3, delay: 2.3, y: 25, ease:'expo.out', stagger: .3})
 
-    $(".grid").isotope({ filter: filterValue });
-  })
-);
+// NAV ITEM
+gsap.from('.nav__logo', {opacity:0, duration: 3, delay: 3.2, y: 25, ease:'expo.out'});
+gsap.from('.nav__item', {opacity: 0, duration: 3, delay: 3.2, y: 25, ease:'expo.out', stagger: .2})
 
-$(".grid").isotope({
-  itemSelector: ".grid-item",
-  layoutMode: "fitRows",
-  transitionDuration: "0.6s",
-});
-
-window.addEventListener("scroll", () => {
-  skillsEffect();
-  countUp();
-});
-
-function checkScroll(el) {
-  let rect = el.getBoundingClientRect();
-  if (window.innerHeight >= rect.top + el.offsetHeight) return true;
-  return false;
-}
-
-function skillsEffect() {
-  if (!checkScroll(skills_wrap)) return;
-  skills_bars.forEach((skill) => (skill.style.width = skill.dataset.progress));
-}
-
-function countUp() {
-  if (!checkScroll(records_wrap)) return;
-  records_numbers.forEach((numb) => {
-    const updateCount = () => {
-      let currentNum = +numb.innerText;
-      let maxNum = +numb.dataset.num;
-      let speed = 100;
-      const increment = Math.ceil(maxNum / speed);
-
-      if (currentNum < maxNum) {
-        numb.innerText = currentNum + increment;
-        setTimeout(updateCount, 1);
-      } else {
-        numb.innerText = maxNum;
-      }
-    };
-
-    setTimeout(updateCount, 400);
-  });
-}
-
-var mySwiper = new Swiper(".swiper-container", {
-  speed: 1100,
-  slidesPerView: 1,
-  loop: true,
-  autoplay: {
-    delay: 5000,
-  },
-  navigation: {
-    prevEl: ".swiper-button-prev",
-    nextEl: ".swiper-button-next",
-  },
-});
+// SOCIAL
+gsap.from('.home__social-icon', {opacity: 0, duration: 3, delay: 4, y: 25, ease:'expo.out', stagger: .2})
